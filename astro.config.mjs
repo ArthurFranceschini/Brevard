c807db207a00216e16c4830f6ab9a120ac77faab
ArthurFranceschini <arthurfranvalerio@gmail.com>
2025-07-28 14:33:17 -0300

    código pronto, aguardando aprovação

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
+  devToolbar:{
+    enabled: false,
+},
+
+integrations: [
     alpinejs()
   ],
 
